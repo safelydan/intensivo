f9741c82e6fc2807c255ffb6b9b55f99979a9de5
daniel araujo <daniellealuau@gmail.com>
2024-02-01 15:47:48 -0300

    ok

--- paginaApple/script.js
+++ paginaApple/script.js
@@ -1,7 +1,8 @@
+// definição de um objeto representando a cor "azul-inverno"
 const azulInverno = {
-    nome: "Azul-inverno",
-    pasta: "imagens-azul-inverno"
-};    
+    nome: "Azul-inverno",      // propriedade: nome da cor
+    pasta: "imagens-azul-inverno"   // propriedade: pasta onde estão armazenadas as imagens dessa cor
+};   
 
 const verdeCipreste = {
     nome: "Verde-cipreste",
@@ -23,4 +24,8 @@ const estelar = {
     pasta: "imagens-estelar"
 };
 
-const minhaLista = [5, 3,4, 4,4,4,4,4]
+// acesso à propriedade "pasta" do objeto "meiaNoite" e não está sendo feito nada com o valor retornado
+meiaNoite.pasta;  
+
+// criação de uma lista chamada "opcoesDeCores" contendo os objetos das cores definidas anteriormente
+const opcoesDeCores = [azulInverno, verdeCipreste, meiaNoite, rosaClaro, estelar];
