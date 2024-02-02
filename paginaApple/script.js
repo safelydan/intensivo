74fa3d4ef328308a8d44ceb6c76f4c679d140ffd
daniel araujo <daniellealuau@gmail.com>
2024-02-02 15:24:30 -0300

    mudando titulo

--- paginaApple/script.js
+++ paginaApple/script.js
@@ -1,3 +1,5 @@
+const imagemVisualizacao = document.getElementById("imagem-visualizacao")
+const tituloProduto = document.getElementById("titulo-produto")
 // definição de um objeto representando a cor "azul-inverno"
 const azulInverno = {
     nome: "Azul-inverno",      // propriedade: nome da cor
@@ -29,3 +31,38 @@ meiaNoite.pasta;
 
 // criação de uma lista chamada "opcoesDeCores" contendo os objetos das cores definidas anteriormente
 const opcoesDeCores = [azulInverno, verdeCipreste, meiaNoite, rosaClaro, estelar];
+const opcoesTamanho = ["41 mm", "45 mm"];
+
+// opcoesTamanho -
+
+// let imagemSelecionada = 1; //esta funcionando mesmo sem ela, averiguar depois o porque
+let tamanhoSelecionado = 1;
+
+
+// função para trocar a imagem visualizada com base na opção selecionada
+function trocarImagem() {
+    // obtém o id da opção de imagem selecionada
+    const idOpcaoSelecionada = document.querySelector('[name="opcao-imagem"]:checked').id;
+    
+    // obtém o número da imagem a ser exibida com base no primeiro caractere do id
+    imagemSelecionada = idOpcaoSelecionada.charAt(0);
+    
+    // atualiza o atributo "src" da imagem de visualização com o caminho da nova imagem
+    imagemVisualizacao.src = `./imagens/opcoes-cores/${opcoesDeCores[0].pasta}/imagem-${imagemSelecionada}.jpeg`;
+}
+
+function trocarTamanho() {
+    // o que a gente quer?
+
+    // atualizar variavel de controle do tamamho da caixa
+    const idTamanhoSelecionado = document.querySelector('[name="opcao-tamanho"]:checked').id;
+    tamanhoSelecionado = idTamanhoSelecionado.charAt(0);
+
+    // mudar o titulo
+    tituloProduto.innerText = `Pulseira loop esportiva azul-inverno para caixa de ${opcoesTamanho[tamanhoSelecionado]}`
+    console.log(tamanhoSelecionado)
+    // `Pulseira loop esportiva azul-inverno para caixa de ${} mm`
+    // mudar o tamanho da imagem de acordo com a opçao escolhida
+
+
+}
